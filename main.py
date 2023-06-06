855ad931e8d92ebe008621edc0235f69f7e4964c
krzymich24 <krzysztof.michalak1999@gmail.com>
2023-06-06 21:29:40 +0200

    Progres
    Adding HTML schema

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from blockchain import Blockchain
 
 app = Flask(__name__)
 blockchain = Blockchain()
+
+#login page
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get the username and password from the login form
+        username = request.form['username']
+        password = request.form['password']
+
+        # Perform your authentication logic here
+        # For simplicity, we'll check for a hardcoded username and password
+        if (username == 'admin' and password == 'passwd')or(username == 'JP2' and password == '2137'):
+            # Redirect to the home page if authentication is successful
+            return redirect(url_for('home'))
+        else:
+            # Render the login page with an error message
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+    else:
+        # Render the login page
+        return render_template('login.html')
+
+#home page
+@app.route('/home')
+def home():
+    # Render the home page
+    return render_template('home.html')
 
 # Mining a new block
 
@@ -23,16 +50,21 @@ def mine_block():
 				'proof': block['proof'],
 				'previous_hash': block['previous_hash']}
 
-	return jsonify(response), 200
+	json_data = json.dumps(response) 
+
+	return render_template('mine.html', json_data=json_data)
 
 # Display blockchain in json format
 
-
 @app.route('/get_chain', methods=['GET'])
 def display_chain():
+    
 	response = {'chain': blockchain.chain,
 				'length': len(blockchain.chain)}
-	return jsonify(response), 200
+
+	json_data = json.dumps(response) 
+	print(json_data)
+	return render_template('get_chain.html', json_data=json_data)
 
 # Check validity of blockchain
 
